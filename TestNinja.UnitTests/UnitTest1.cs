e858f6c69389efa3307edfba15649a340ae5a161
Hidayat arghandabi <hidayatarg@gmail.com>
2019-03-12 10:33:04 +0300

    Created the First Unit Test For Reservation

---
+++ TestNinja.UnitTests/UnitTest1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TestNinja.Fundamentals;
+
+namespace TestNinja.UnitTests
+{
+    [TestClass]
+    public class ReservationTests
+    {
+        [TestMethod]
+        public void CanBeCancelledBy_UserIsAdmin_ReturnsTrue()
+        {
+            // Arrange (Prepare object we test)
+            var reservation = new Reservation();
+            
+            // Act (Call method we want to test)
+            var result = reservation.CanBeCancelledBy(new User { IsAdmin = true } );
+
+            // Asset (result and verify its correct)
+            Assert.IsTrue(result);
+
+        }
+    }
+}
